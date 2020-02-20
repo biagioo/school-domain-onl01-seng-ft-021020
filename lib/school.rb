97f2774923a3ac16f5c1ece70525d33de6a14707
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-20 14:17:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,7 +8,7 @@ class School
   
   def add_student(name, grade) 
    if roster.has_key?(grade) 
-     roster[grade] << [name]
+     roster[grade] << name
    else 
      roster[grade] = [name]
   end 
